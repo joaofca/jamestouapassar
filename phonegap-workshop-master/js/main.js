59ff4afa95f34306fed5c304580bd1aeb48870ad
joaofca <joaofca@hotmail.com>
2013-11-30 23:28:51 +0000

    game

--- phonegap-workshop-master/js/main.js
+++ phonegap-workshop-master/js/main.js
@@ -1,23 +1,40 @@
-var app = {
+function alertSeveralTimes() {
+    alert("Hello!");
+    alert("Look, we can run several messages in a row.");
+    alert("Annoyed yet?");
+}
 
-    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
-    },
+function changeCanvasSize() {
+	var gameCanvas = document.getElementById("gameCanvas");
+	gameCanvas.width = 600;
+    gameCanvas.height = 800;
+	
+	var avatarImage = new Image();
+	avatarImage.src = "http://cdn.tutsplus.com/active/uploads/legacy/tuts/342_html5AvoiderGameTutorial/tutorial/avatar.png";
+	
+	gameCanvas.getContext("2d").drawImage(avatarImage, Math.random() * 100, Math.random() * 100);
+}
 
-    initialize: function() {
-        this.store = new MemoryStore();
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    }
+function drawAvatar() {
+    var gameCanvas = document.getElementById("gameCanvas");
+    var avatarImage = new Image();
+     
+    avatarImage.src = "http://cdn.tutsplus.com/active/uploads/legacy/tuts/342_html5AvoiderGameTutorial/tutorial/avatar.png";
+    gameCanvas.getContext("2d").drawImage(avatarImage, Math.random() * 100, Math.random() * 100);
+	
+	gameCanvas.addEventListener("mousemove", redrawAvatar);
+}
 
-};
-
-app.initialize();
+function redrawAvatar(mouseEvent) {
+	var gameCanvas = document.getElementById("gameCanvas");
+    var avatarImage = new Image();
+	var enemyImage = new Image();
+     
+    avatarImage.src = "http://cdn.tutsplus.com/active/uploads/legacy/tuts/342_html5AvoiderGameTutorial/tutorial/avatar.png";
+	enemyImage.src = "http://cdn.tutsplus.com/active/uploads/legacy/tuts/342_html5AvoiderGameTutorial/tutorial/enemy.png";
+    gameCanvas.getContext("2d").drawImage(avatarImage, mouseEvent.offsetX, mouseEvent.offsetY);
+    gameCanvas.getContext("2d").drawImage(enemyImage, 250, 150);
+	// if (mouseEvent.offsetX > 150 && mouseEvent.offsetX < 250 && mouseEvent.offsetY > 100 && mouseEvent.offsetY < 200) {
+        // alert("Stay out of the center!");
+    //}
+}
